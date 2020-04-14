0d176499f39d69b108825d7abc843ab30feae1dd
Telheb <garciaemmanuel@hotmail.com>
2020-04-14 11:47:01 +0200

    modif de ma branch css_form_film

--- formulaire_film_serie.php
+++ formulaire_film_serie.php
@@ -6,9 +6,9 @@
 
     <form action="" method="" class="form-film-serie">
 
-        <a href="#modal1" class="js-modal a-modal">Ouvrir la boite modal</a>
+        <a href="#modal1" class="js-modal a-modal">Ouvrir la boite modal <br> Pour remplire la première partie</a>
         <aside id="modal1" class="modal" aria-hidden="true" role="dialog" aria-modal="false" aria-labelledby="premierFormulaire" style="display: none"> <!-- definir le nom de mes modal --> 
-            <h3 id="premierFormulaire">1er partie du formulaire</h3>
+            <h3 id="premierFormulaire" class="titre-form-film-serie">Premier partie du formulaire</h3>
             <div class="modal-wrapper js-modal-stop">
                <label for="nom">nom : </label>
                <input type="text" name="nom" id="nom"><br>
@@ -16,9 +16,9 @@
             <button class="js-modal-close">Fermer la boite modal</button>
         </aside>
 
-        <a href="#modal1" class="js-modal a-modal">Ouvrir la boite modal</a>
+        <a href="#modal1" class="js-modal a-modal">Ouvrir la boite modal <br> Pour remplire la deuxième partie</a>
         <aside id="modal1" class="modal" aria-hidden="true" role="dialog" aria-modal="false" aria-labelledby="premierFormulaire" style="display: none"> <!-- definir le nom de mes modal --> 
-            <h3 id="premierFormulaire">1er partie du formulaire</h3>
+            <h3 id="premierFormulaire" class="titre-form-film-serie">Deuxième partie du formulaire</h3>
             <div class="modal-wrapper js-modal-stop">
                <label for="nom">nom : </label>
                <input type="text" name="nom" id="nom"><br>
@@ -26,9 +26,9 @@
             <button class="js-modal-close">Fermer la boite modal</button>
         </aside>
 
-        <a href="#modal1" class="js-modal a-modal">Ouvrir la boite modal</a>
+        <a href="#modal1" class="js-modal a-modal">Ouvrir la boite modal <br> Pour remplire la troisième partie</a>
         <aside id="modal1" class="modal" aria-hidden="true" role="dialog" aria-modal="false" aria-labelledby="premierFormulaire" style="display: none"> <!-- definir le nom de mes modal --> 
-            <h3 id="premierFormulaire">1er partie du formulaire</h3>
+            <h3 id="premierFormulaire" class="titre-form-film-serie">Troisième partie du formulaire</h3>
             <div class="modal-wrapper js-modal-stop">
                <label for="nom">nom : </label>
                <input type="text" name="nom" id="nom"><br>
@@ -36,9 +36,9 @@
             <button class="js-modal-close">Fermer la boite modal</button>
         </aside>
  
-        <input type="submit" name="submit" value="Envoyez">
+        <input type="submit" name="submit" value="Une fois le formulaire fini, vous pouvez le valider en cliquant ici">
     </form>
 
-    <script src="js/modal_form.js"></script>
+
 </body>
 </html>
