4a931440912fcf7578107d86058e27e57afa1c75
Telheb <garciaemmanuel@hotmail.com>
2020-04-14 09:04:50 +0200

    remise à jour fichier_2

---
+++ formulaire_film_serie.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/form_style.css">
+    <title>Formulaire</title>
+</head>
+<body>
+    <form action="" method="">
+        <a href="#modal1" class="js-modal">Ouvrir la boite modal</a>
+        <aside id="modal1" class="modal" aria-hidden="true" role="dialog" aria-modal="false" aria-labelledby="premierFormulaire" style="display: none"> <!-- definir le nom de mes modal --> 
+            <h3 id="premierFormulaire">1er partie du formulaire</h3>
+            <div class="modal-wrapper js-modal-stop">
+               <label for="nom">nom : </label>
+               <input type="text" name="nom" id="nom"><br>
+            </div>
+            <button class="js-modal-close">Fermer la boite modal</button>
+        </aside>
+ 
+        <input type="submit" name="submit" value="Envoyez">
+    </form>
+    <script src="js/modal_form.js"></script>
+</body>
+</html>
