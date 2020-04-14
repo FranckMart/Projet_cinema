04518e0a16c9e53e0277f1dd0b9cb34112a97963
Franck <franckmart22@gmail.com>
2020-04-14 11:50:49 +0200

    mise à jour contenu dossier projet

--- formulaire_film_serie.php
+++ formulaire_film_serie.php
@@ -1,44 +1,62 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/kit_UI.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/form_style.css">
+    <title><?= $title ?></title>
+</head>
+
 <?php
-    require "include/header.php";
-    require "include/navbar.php";
-?>    
+
+require "include/header_nav.php";
+?>
 
 
-    <form action="" method="">
+<form action="" method="">
 
-        <a href="#modal1" class="js-modal a-modal">Ouvrir la boite modal</a>
-        <aside id="modal1" class="modal" aria-hidden="true" role="dialog" aria-modal="false" aria-labelledby="premierFormulaire" style="display: none"> <!-- definir le nom de mes modal --> 
-            <h3 id="premierFormulaire">1er partie du formulaire</h3>
-            <div class="modal-wrapper js-modal-stop">
-               <label for="nom">nom : </label>
-               <input type="text" name="nom" id="nom"><br>
-            </div>
-            <button class="js-modal-close">Fermer la boite modal</button>
-        </aside>
+    <a href="#modal1" class="js-modal a-modal">Ouvrir la boite modal</a>
+    <aside id="modal1" class="modal" aria-hidden="true" role="dialog" aria-modal="false" aria-labelledby="premierFormulaire" style="display: none">
+        <!-- definir le nom de mes modal -->
+        <h3 id="premierFormulaire">1er partie du formulaire</h3>
+        <div class="modal-wrapper js-modal-stop">
+            <label for="nom">nom : </label>
+            <input type="text" name="nom" id="nom"><br>
+        </div>
+        <button class="js-modal-close">Fermer la boite modal</button>
+    </aside>
 
-        <a href="#modal1" class="js-modal">Ouvrir la boite modal</a>
-        <aside id="modal1" class="modal" aria-hidden="true" role="dialog" aria-modal="false" aria-labelledby="premierFormulaire" style="display: none"> <!-- definir le nom de mes modal --> 
-            <h3 id="premierFormulaire">1er partie du formulaire</h3>
-            <div class="modal-wrapper js-modal-stop">
-               <label for="nom">nom : </label>
-               <input type="text" name="nom" id="nom"><br>
-            </div>
-            <button class="js-modal-close">Fermer la boite modal</button>
-        </aside>
+    <a href="#modal1" class="js-modal">Ouvrir la boite modal</a>
+    <aside id="modal1" class="modal" aria-hidden="true" role="dialog" aria-modal="false" aria-labelledby="premierFormulaire" style="display: none">
+        <!-- definir le nom de mes modal -->
+        <h3 id="premierFormulaire">1er partie du formulaire</h3>
+        <div class="modal-wrapper js-modal-stop">
+            <label for="nom">nom : </label>
+            <input type="text" name="nom" id="nom"><br>
+        </div>
+        <button class="js-modal-close">Fermer la boite modal</button>
+    </aside>
 
-        <a href="#modal1" class="js-modal">Ouvrir la boite modal</a>
-        <aside id="modal1" class="modal" aria-hidden="true" role="dialog" aria-modal="false" aria-labelledby="premierFormulaire" style="display: none"> <!-- definir le nom de mes modal --> 
-            <h3 id="premierFormulaire">1er partie du formulaire</h3>
-            <div class="modal-wrapper js-modal-stop">
-               <label for="nom">nom : </label>
-               <input type="text" name="nom" id="nom"><br>
-            </div>
-            <button class="js-modal-close">Fermer la boite modal</button>
-        </aside>
- 
-        <input type="submit" name="submit" value="Envoyez">
-    </form>
+    <a href="#modal1" class="js-modal">Ouvrir la boite modal</a>
+    <aside id="modal1" class="modal" aria-hidden="true" role="dialog" aria-modal="false" aria-labelledby="premierFormulaire" style="display: none">
+        <!-- definir le nom de mes modal -->
+        <h3 id="premierFormulaire">1er partie du formulaire</h3>
+        <div class="modal-wrapper js-modal-stop">
+            <label for="nom">nom : </label>
+            <input type="text" name="nom" id="nom"><br>
+        </div>
+        <button class="js-modal-close">Fermer la boite modal</button>
+    </aside>
 
-    <script src="js/modal_form.js"></script>
+    <input type="submit" name="submit" value="Envoyez">
+</form>
+
+
+<?php include("include/script_js.php"); ?>
 </body>
+
 </html>
---
+++ include/header_nav.php
@@ -0,0 +1,54 @@
+<header>
+    <nav id="header_nav">
+        <div id="logo">
+            <a href="index.php"><img src="../img/logo.png" alt="logo myIMMO"></a>
+        </div>
+        <div id="content-header-right">
+            <ul class="nav_categorie">
+                <li><a href="news.php">News</a></li>
+                <div class="dropdown">
+                    <li><a href="critique.php">Film/série</a></li>
+                        <div class="dropdown-child">
+                            <div class="bloc">
+                                <ul>
+                                    <li><a href="#">Action</a></li>
+                                    <li><a href="#">Aventure</a></li>
+                                    <li><a href="#">Animation</a></li>
+                                    <li><a href="#">Drame</a></li>
+                                    <li><a href="#">Comédie</a></li>
+                                </ul>
+                            </div>
+                            <div class="bloc">
+                                <ul>
+                                    <li><a href="#">Famille</a></li>
+                                    <li><a href="#">Guerre</a></li>
+                                    <li><a href="#">Science-fiction</a></li>
+                                    <li><a href="#">Musical</a></li>
+                                    <li><a href="#">Policer</a></li>
+                                </ul>
+                            </div>
+                            <a href="#" class="showMore">Voir Plus >></a>
+                        </div>
+
+                </div>
+                <li><a href="formulaire_film_serie.php">Formulaire</a>
+                <li><a href="annonces.php">Nous contacter</a></li>
+                <li><input type="search" name="recherche" id="recherche" placeholder="...Search"></li>
+                <div class="login">
+                    <!-- Button trigger modal -->
+                    <li>
+                        <a href="" data-toggle="modal" data-target="#exampleModal">
+                            <!-- <div>Icons made by <a href="https://www.flaticon.com/authors/srip" title="srip">srip</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>-->
+                            <img src="IMG/login.png" alt="login">
+                        </a>
+                    </li>
+                </div>
+</ul>
+                <div class="burger">
+                    <div class="line1"></div>
+                    <div class="line2"></div>
+                    <div class="line3"></div>
+                </div>
+        </div>
+    </nav>
+</header>
