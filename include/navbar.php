6aa2d5c07632a84d39cc70468adc286ab4a8c9c5
Franck <franckmart22@gmail.com>
2020-04-14 10:37:05 +0200

    modif headerNav

--- include/navbar.php
+++ include/navbar.php
@@ -1,50 +1,53 @@
-<nav>
-        <div id="logo">
-            <a href="accueil_or_connection.html"><img src="../img/logo.png" alt="logo myIMMO"></a>
-        </div>
-        <div id="content-header-right">
-            <ul class="nav_categorie">
-            </li><a href="formulaire_film_serie.php">Formulaire</a><li>
-                <li><a href="accueil.html">News</a></li>
-                <div class="dropdown">
-                    </li><a href="tuto.html">Film/série</a><li>
-                        <div class="dropdown-child">
-                            <div class="bloc">
-                                <ul>
-                                    <li><a href="#">Action</a></li>
-                                    <li><a href="#">Aventure</a></li>
-                                    <li><a href="#">Animation</a></li>
-                                    <li><a href="#">Drame</a></li>
-                                    <li><a href="#">Comédie</a></li> 
-                                </ul>
-                            </div>
-                            <div class="bloc">
-                                <ul>
-                                    <li><a href="#">Famille</a></li>
-                                    <li><a href="#">Guerre</a></li>
-                                    <li><a href="#">Science-fiction</a></li>
-                                    <li><a href="#">Musical</a></li>
-                                    <li><a href="#">Policer</a></li>
-                                </ul>
-                            </div>
-                            <a href="#" class="showMore">Voir Plus >></a>
+<nav id="header_nav">
+    <div id="logo">
+        <a href="accueil_or_connection.html"><img src="../img/logo.png" alt="logo myIMMO"></a>
+    </div>
+    <div id="content-header-right">
+        <ul class="nav_categorie">
+            <li><a href="accueil.html">News</a></li>
+            <div class="dropdown">
+                </li><a href="tuto.html">Film/série</a>
+                <li>
+                    <div class="dropdown-child">
+                        <div class="bloc">
+                            <ul>
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Aventure</a></li>
+                                <li><a href="#">Animation</a></li>
+                                <li><a href="#">Drame</a></li>
+                                <li><a href="#">Comédie</a></li>
+                            </ul>
                         </div>
-                </div>
-                <li><a href="annonces.html">Nous contacter</a></li>
-                <li><input type="search" name="recherche" id="recherche" placeholder="...Search"></li>
-                <div class="login">
-                    <!-- Button trigger modal -->
-                    <li>
-                        <a href="" data-toggle="modal" data-target="#exampleModal">
-                            <!-- <div>Icons made by <a href="https://www.flaticon.com/authors/srip" title="srip">srip</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>-->
-                            <img src="IMG/login.png" alt="login">
-                        </a>
-                    </li>
-                </div>
-                <div class="burger">
-                    <div class="line1"></div>
-                    <div class="line2"></div>
-                    <div class="line3"></div>
-                </div>
-        </div>
-    </nav>
+                        <div class="bloc">
+                            <ul>
+                                <li><a href="#">Famille</a></li>
+                                <li><a href="#">Guerre</a></li>
+                                <li><a href="#">Science-fiction</a></li>
+                                <li><a href="#">Musical</a></li>
+                                <li><a href="#">Policer</a></li>
+                            </ul>
+                        </div>
+                        <a href="#" class="showMore">Voir Plus >></a>
+                    </div>
+
+            </div>
+            <li><a href="formulaire_film_serie.php">Formulaire</a>
+            <li><a href="annonces.html">Nous contacter</a></li>
+            <li><input type="search" name="recherche" id="recherche" placeholder="...Search"></li>
+            <div class="login">
+                <!-- Button trigger modal -->
+                <li>
+                    <a href="" data-toggle="modal" data-target="#exampleModal">
+                        <!-- <div>Icons made by <a href="https://www.flaticon.com/authors/srip" title="srip">srip</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>-->
+                        <img src="IMG/login.png" alt="login">
+                    </a>
+                </li>
+            </div>
+            <div class="burger">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+        </ul>
+    </div>
+</nav>
