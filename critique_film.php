47230f6057c12fb0fbf67abf7360fcf1aff3d5a9
Telheb <garciaemmanuel@hotmail.com>
2020-04-16 16:35:29 +0200

    ajoute page critique film

---
+++ critique_film.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/kit_UI.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/modal_user.css">
+    <link rel="stylesheet" href="css/accueil.css">
+    <title></title>
+</head>
+<body>
+    
+</body>
+</html>
