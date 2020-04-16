430cff3fb176d5b66659ac83297c812d980ce8f4
Telheb <garciaemmanuel@hotmail.com>
2020-04-16 15:42:23 +0200

    fixe probleme Js et css à jour

--- JS/modal_form_film_serie.js
+++ JS/modal_form_film_serie.js
@@ -4,8 +4,10 @@ const openModal = function(e){
     e.preventDefault();
     const target = document.querySelector(e.target.getAttribute('href'));
     target.style.display = null;
+    target.style.backgroundColor = "black";
     target.removeAttribute('aria-hidden');
     target.setAttribute('aria-modal', 'true');
+
     modal = target;
     modal.addEventListener('click', closeModal);
     modal.querySelector('.js-modal-close').addEventListener('click', closeModal);
@@ -13,9 +15,12 @@ const openModal = function(e){
 }
 
 const closeModal = function (e){
+    const targetColor = document.getElementsByClassName('a-modal');
     if(modal === null) return;
     e.preventDefault();
     modal.style.display = "none";
+    targetColor.style.backgroundColor = "white";
+    tagetColor.style.backgroundColor = "black";
     modal.setAttribute('aria-hidden', 'true');
     modal.removeAttribute('aria-modal');
     modal.removeEventListener('click', closeModal);
