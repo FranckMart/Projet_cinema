ecb7695a05c169a4c73b4e2c78b5d3213705ea1b
Telheb <garciaemmanuel@hotmail.com>
2020-04-17 11:51:34 +0200

    fix bub meta charset

--- include/bdd.php
+++ include/bdd.php
@@ -4,7 +4,7 @@
 require_once("include/config.php");
 try{
 	$bdd = new PDO("mysql:host=".$host.";dbname=".$dbname, $userBdd, $passBdd);
-
+	$bdd->exec('SET NAMES utf8');
 	
 } catch (Exception $exception){
 	$messageError = $exception->getMessage();
