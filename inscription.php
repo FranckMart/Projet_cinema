c5348d376064683ebc83cb4c25023f062c4a78c8
Franck <franckmart22@gmail.com>
2020-07-24 18:52:23 +0200

    css chemin modif

--- inscription.php
+++ inscription.php
@@ -12,11 +12,11 @@ if (isset($_POST['deco'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/kit_UI.css">
-    <link rel="stylesheet" href="css/header.css">
-    <link rel="stylesheet" href="css/modal_user.css">
-    <link rel="stylesheet" href="css/inscription.css">
-    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="CSS/kit_UI.css">
+    <link rel="stylesheet" href="CSS/header.css">
+    <link rel="stylesheet" href="CSS/modal_user.css">
+    <link rel="stylesheet" href="CSS/inscription.css">
+    <link rel="stylesheet" href="CSS/footer.css">
     <title>Inscription</title>
 </head>
 
